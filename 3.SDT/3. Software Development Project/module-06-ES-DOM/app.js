0cdb757ed14ce6e42ee282432cbff4667cf9577c
Khyruzzaman <khyruzzaman999@gmail.com>
2026-03-01 21:39:34 +0600

    SDT Week-2 JavaScript

---
+++ 3.SDT/3. Software Development Project/module-06-ES-DOM/app.js
@@ -0,0 +1,18 @@
+// Arrow Function
+const sum2 = (num1, num2) => {
+    return num1 + num2;
+}
+
+let output = sum2(10, 20)
+console.log(output)
+
+
+// Find Method
+// it will just return 1 object
+
+// Filter Method
+// it will return 1 more object as array
+
+// Map Method
+
+// ForEach Method
