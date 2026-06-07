48aa6ad116cfc68aa6e728d699f4172316acc44a
MD.KHYRUZZAMAN <62590127+mdkhyruzzaman@users.noreply.github.com>
2026-06-07 20:53:45 +0600

    SDT model 6 completed

--- 3.SDT/3.2 SDP (Git, Django)/task-management/tasks/views.py
+++ 3.SDT/3.2 SDP (Git, Django)/task-management/tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from tasks.forms import TaskForm
-from tasks.models import Employee
+from tasks.forms import TaskForm, TaskModelForm
+from tasks.models import Employee, Task
 
 # Create your views here.
 def manager_dashboard(request):
@@ -13,8 +13,40 @@ def user_dashboard(request):
 def test_page(request):
     return render(request, "test.html")
 
+''' Django Normal Form '''
+# def create_task(request):
+#     employees = Employee.objects.all()
+#     form = TaskForm(employees=employees)
+
+#     if request.method == 'POST':
+#         form = TaskForm(request.POST, employees=employees)
+#         if form.is_valid():
+#             data = form.cleaned_data
+#             title = data.get('title')
+#             description = data.get('description')
+#             due_date = data.get('due_date')
+#             assigned_to = data.get('assigned_to')
+
+#             task = Task.objects.create(title=title, description=description, due_date=due_date)
+
+#             # Assigned To
+#             for emp_id in assigned_to:
+#                 employee = Employee.objects.get(id=emp_id)
+#                 task.assigned_to.add(employee)
+
+#             return HttpResponse("Task Added Successfully.")
+
+#     context = {"form": form}
+#     return render(request, "task_form.html", context)
+
+''' Django Model Form '''
 def create_task(request):
-    employees = Employee.objects.all()
-    form = TaskForm(employees=employees)
+    form = TaskModelForm()
     context = {"form": form}
+
+    if request.method == 'POST':
+        form = TaskModelForm(request.POST)
+        form.save()
+        context = {"form": form, "message": "Task Added Successfully."}
+
     return render(request, "task_form.html", context)
