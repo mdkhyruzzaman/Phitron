2b49462ab6691ad9b45eadb95305720f583abb8a
MD.KHYRUZZAMAN <62590127+mdkhyruzzaman@users.noreply.github.com>
2026-06-07 09:00:02 +0600

    SDK module 6

--- 3.SDT/3.2 SDP (Git, Django)/task-management/tasks/urls.py
+++ 3.SDT/3.2 SDP (Git, Django)/task-management/tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from tasks.views import manager_dashboard, user_dashboard, test_page
+from tasks.views import manager_dashboard, user_dashboard, test_page, create_task
 
 urlpatterns = [
     path('manager-dashboard/', manager_dashboard),
     path('user-dashboard/', user_dashboard),
-    path('test-page', test_page)
+    path('test-page/', test_page),
+    path('create-task/', create_task)
 ]
--- 3.SDT/3.2 SDP (Git, Django)/task-management/tasks/views.py
+++ 3.SDT/3.2 SDP (Git, Django)/task-management/tasks/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tasks.forms import TaskForm
+from tasks.models import Employee
 
 # Create your views here.
 def manager_dashboard(request):
@@ -10,3 +12,9 @@ def user_dashboard(request):
 
 def test_page(request):
     return render(request, "test.html")
+
+def create_task(request):
+    employees = Employee.objects.all()
+    form = TaskForm(employees=employees)
+    context = {"form": form}
+    return render(request, "task_form.html", context)
